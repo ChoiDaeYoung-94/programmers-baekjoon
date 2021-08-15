21e85abb35be6345e06fb7433d2c69dadf205bbe
ChoiDaeYoung <chleodud1410@gmail.com>
2021-08-15 17:20:12 +0900

    .

---
+++ Level_1/행렬의 덧셈.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public int[,] solution(int[,] arr1, int[,] arr2)
+    {
+        int[,] answer = arr1;
+        for (int i = 0; i < answer.GetLength(0); i++)
+            for (int j = 0; j < answer.GetLength(1); j++)
+                answer[i, j] += arr2[i, j];
+        return answer;
+    }
+}
