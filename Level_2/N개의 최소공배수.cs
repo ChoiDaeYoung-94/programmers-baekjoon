b7a5f06e034aabb2d8b90c93cce737dca042b1b9
ChoiDaeYoung <chleodud1410@gmail.com>
2021-10-20 23:55:55 +0900

    .

---
+++ Level_2/N개의 최소공배수.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int solution(int[] arr)
+    {
+        int answer = LeastCommonMultiple(arr);
+        return answer;
+    }
+
+    int LeastCommonMultiple(int[] arr)
+    {
+        int lcm = 1;
+        bool isFinished = false;
+
+        while (!isFinished)
+        {
+            isFinished = true;
+
+            for (int i = -1; ++i < arr.Length;)
+                if (lcm % arr[i] != 0)
+                {
+                    ++lcm;
+                    isFinished = false;
+                }
+        }
+
+        return lcm;
+    }
+}
