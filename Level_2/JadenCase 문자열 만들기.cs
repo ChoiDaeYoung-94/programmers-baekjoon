bfc193085c80c2b8f62275de05d53f2c05c17128
ChoiDaeYoung <chleodud1410@gmail.com>
2021-10-21 23:30:09 +0900

    .

---
+++ Level_2/JadenCase 문자열 만들기.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        StringBuilder answer = new StringBuilder();
+        string[] _splitS = s.ToLower().Split(new char[] { ' ' });
+
+        for (int i = -1; ++i < _splitS.Length;)
+        {
+            if (_splitS[i] != "")
+            {
+                string first = _splitS[i].Substring(0, 1).ToUpper();
+
+                if (_splitS[i].Length > 1)
+                    answer.Append(first + _splitS[i].Substring(1) + " ");
+                else
+                    answer.Append(first + " ");
+            }
+            else
+                answer.Append(" ");
+        }
+        answer.Remove(answer.Length - 1, 1);
+
+        return answer.ToString();
+    }
+}
