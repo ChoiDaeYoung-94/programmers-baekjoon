d02a6e7674647514a9bd6547c74cce8400d7edd5
ChoiDaeYoung <chleodud1410@gmail.com>
2021-10-28 00:39:48 +0900

    .

---
+++ Level_2/다음 큰 숫자.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    public int solution(int n)
+    {
+        int answer = n;
+        int _count = Count(Convert.ToString(n, 2));
+
+        while (true)
+        {
+            if (_count == Count(Convert.ToString(++answer, 2)))
+                break;
+        }
+
+        return answer;
+    }
+
+    int Count(string str)
+    {
+        int count = 0;
+        for (int i = -1; ++i < str.Length;)
+            if (str[i] == '1')
+                ++count;
+
+        return count;
+    }
+
+    //.Where(x => x == '1').Count()
+}
