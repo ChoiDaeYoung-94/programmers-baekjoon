658f306bbafff1cdb18efb404882cf765c157c1c
ChoiDaeYoung <chleodud1410@gmail.com>
2021-11-04 00:02:37 +0900

    .

---
+++ Level_2/스킬트리.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public int solution(string skill, string[] skill_trees)
+    {
+        int answer = 0;
+
+        for (int i = -1; ++i < skill_trees.Length;)
+        {
+            string cur_skill = skill_trees[i];
+
+            StringBuilder selectSkill = new StringBuilder();
+            for (int j = -1; ++j < cur_skill.Length;)
+                if (skill.Contains(cur_skill[j].ToString()))
+                    selectSkill.Append(cur_skill[j].ToString());
+
+            bool check = false;
+            for (int x = -1; ++x < selectSkill.Length;)
+                if (skill[x] != selectSkill[x])
+                {
+                    check = true;
+                    break;
+                }
+            if (check)
+                continue;
+
+            ++answer;
+        }
+
+        return answer;
+    }
+}
