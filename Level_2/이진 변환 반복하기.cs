0dde7b2f270c6f29e7add9628bd0c0c8c180d853
ChoiDaeYoung <chleodud1410@gmail.com>
2021-11-14 00:28:54 +0900

    .

---
+++ Level_2/이진 변환 반복하기.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(string s)
+    {
+        int[] answer = new int[2];
+
+        while (s != "1")
+        {
+            answer[0] += 1;
+
+            int oldLength = s.Length;
+            int temp = s.ToList().RemoveAll(x => x == '0');
+
+            answer[1] += temp;
+
+            s = Convert.ToString(oldLength - temp, 2);
+        }
+
+        return answer;
+    }
+}
