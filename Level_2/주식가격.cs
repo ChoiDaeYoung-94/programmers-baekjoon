d525b2b39ad1add0ca254d0dac0c6cee273f31ab
ChoiDaeYoung <chleodud1410@gmail.com>
2021-12-13 22:29:22 +0900

    .

---
+++ Level_2/주식가격.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int[] prices)
+    {
+        int length = prices.Length;
+        int[] answer = new int[length];
+
+        for (int i = -1; ++i < length;)
+            for (int j = i; ++j < length;)
+            {
+                answer[i] += 1;
+
+                if (prices[j] < prices[i])
+                    break;
+            }
+
+        return answer;
+    }
+}
